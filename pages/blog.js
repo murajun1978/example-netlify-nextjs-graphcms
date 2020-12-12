f1fc3a4315d7cbdb324b4a659af003e78d196066
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2020-12-12 09:21:24 +0900

    Modify blog style

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,23 @@
 import { GraphQLClient } from "graphql-request";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import { query } from '../graphql/queries/posts'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 export const getStaticProps = async () => {
   const client = new GraphQLClient(process.env.GRAPHCMS_API, {
@@ -18,12 +35,30 @@ export const getStaticProps = async () => {
 }
 
 const Blog = ({ posts }) => {
+  const classes = useStyles();
+
   return (
     <>
       {
         posts.map(post => {
           return (
-            <div>{post.title}</div>
+            <Card key={post.id} className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={post.coverImage.url}
+                  title={post.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {post.excerpt}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           )
         })
       }
